4967a53e2d979f7b604c8ea36ed321bd088c2969
bauz24 <bautizumpa@gmail.com>
2026-04-29 16:54:38 -0300

    include arreglos

--- EDA1/libs/listas/implementaciones/listas_arreglos.c
+++ EDA1/libs/listas/implementaciones/listas_arreglos.c
@@ -1,7 +1,7 @@
-#include ".\listas.h"
+#include "../headers/listas.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "..\Tipos\tipo_elemento.h"
+#include "../../tipoElemento/headers/tipo_elemento.h"
 
 // static const int TAMANIO_MAXIMO = 100;  Pasado al .h
 
