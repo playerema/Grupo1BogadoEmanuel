7197213c2876a930469c7b68b69ca309e0d2389a
playerema <63883283+playerema@users.noreply.github.com>
2026-05-27 22:59:22 -0300

    listo

---
+++ EDA1/03-trabajoPractico-Pilas/ejercicio6.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "tp_pilas.h"
+#include <string.h>
+#include "pilas_arreglos.c"
+#include "tipo_elemento.c"
+
+Pila p_ej6_eliminarclave(Pila p, int clave){
+    Pila paux = p_crear();
+    Pila resaux = p_crear();
+    Pila res = p_crear();
+
+    while(!p_es_vacia(p)){
+        TipoElemento te= p_desapilar(p);
+
+        p_apilar(paux,te);
+
+        if(te->clave!=clave){
+            p_apilar(resaux,te);
+        }  
+    }
+
+    while(!p_es_vacia(paux)){
+        p_apilar(p,p_desapilar(paux));
+    }
+
+    while(!p_es_vacia(resaux)){
+        p_apilar(res, p_desapilar(resaux));
+    }
+
+    return res;
+
+}//O(n)
+
+void p_ej6_eliminarclaveAux(Pila p, int clave, Pila res){
+
+    if(p_es_vacia(p)){
+        return;
+    }
+
+    TipoElemento te = p_desapilar(p);
+
+    eliminarAux(p, clave, res);
+
+    p_apilar(p, te);
+
+    if(te->clave != clave){
+        p_apilar(res, te);
+    }
+}
+
+Pila p_ej6_eliminarclave2(Pila p, int clave){
+
+    Pila res = p_crear();
+
+    p_ej6_eliminarclaveAux(p, clave, res);
+
+    return res;
+}
+
+
+int main(){
+
+    
+    return 0;
+}
