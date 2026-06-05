27c6a3d3adbb042ec2b8ec9ad77e62d31e63f6ff
Thomas Balvidares <thomasbalvi016@gmail.com>
2026-06-05 14:11:17 -0300

    Update pilas_arreglos.c

--- EDA1/libs/pilas/implementaciones/pilas_arreglos.c
+++ EDA1/libs/pilas/implementaciones/pilas_arreglos.c
@@ -11,7 +11,7 @@ struct PilaRep {
 
 Pila p_crear() {
     Pila nueva_pila = (Pila) malloc(sizeof(struct PilaRep));
-    nueva_pila->valores = calloc((TAMANIO_MAXIMO+1), sizeof(TipoElemento));
+    nueva_pila->valores = calloc((TAMANIO_MAXIMO_PILAS+1), sizeof(TipoElemento));
     nueva_pila->tope = 0;
     return nueva_pila;
 }
@@ -51,7 +51,7 @@ bool p_es_vacia(Pila pila) {
 
 
 bool p_es_llena(Pila pila) {
-    return (pila->tope == TAMANIO_MAXIMO);
+    return (pila->tope == TAMANIO_MAXIMO_PILAS);
 }
 
 
